74c535e43e9794ae0b478c944a06ae92f1643472
bibek@dzangolab <bibek@dzangolab.com>
2022-04-20 19:23:54 +0545

    refactor: update code

--- projects/client/src/pages/profile/index.tsx
+++ projects/client/src/pages/profile/index.tsx
@@ -7,9 +7,9 @@ import Page from "../../common/components/templates/page";
 import styles from "./index.module.css"
 
 export default function Profile(): JSX.Element {
-    const stringifiedUser = useSelector((state: RootState) => state.login.user)
+    const stringifiedUser = useSelector((state: RootState) => state.login.user).toString()
 
-    const user = JSON.parse(stringifiedUser.toString())
+    const user = JSON.parse(stringifiedUser)
     return (
         <Page>
             <h1>Profile</h1>
